1a8214c3cbf5697b6961ebde88b4e686b45fba57
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-02-15 09:39:41 -0300

    Implementando usecase

--- src/main/java/com/trepudox/music/core/usecase/impl/GetArtistModelByIdUseCase.java
+++ src/main/java/com/trepudox/music/core/usecase/impl/GetArtistModelByIdUseCase.java
@@ -1,8 +1,10 @@
 package com.trepudox.music.core.usecase.impl;
 
+import com.trepudox.music.core.exception.NotFoundException;
 import com.trepudox.music.core.usecase.IGetArtistModelByIdUseCase;
 import com.trepudox.music.dataprovider.model.ArtistModel;
 import com.trepudox.music.dataprovider.repository.ArtistRepository;
+import com.trepudox.music.entity.enums.EntityEnums;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +15,8 @@ public class GetArtistModelByIdUseCase implements IGetArtistModelByIdUseCase {
     private final ArtistRepository artistRepository;
 
     public ArtistModel get(Long id) {
-        return artistRepository.getById(id);
+        return artistRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(EntityEnums.ARTIST, id));
     }
 
 }
