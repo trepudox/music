4e9a01910db171915ab112daafd418815f83230d
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-02-15 16:40:59 -0300

    Adicionando logs no ExceptionHandler

--- src/main/java/com/trepudox/music/core/exception/handler/ApiExceptionHandler.java
+++ src/main/java/com/trepudox/music/core/exception/handler/ApiExceptionHandler.java
@@ -5,6 +5,8 @@ import com.trepudox.music.core.exception.NotFoundException;
 import com.trepudox.music.entrypoint.response.ErrorResponse;
 import com.trepudox.music.entrypoint.response.global.GlobalResponse;
 import com.trepudox.music.util.factory.GlobalResponseFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,26 +22,47 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedList;
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final String EXCEPTION_MESSAGE = "EXCEPTION: {}";
+
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<GlobalResponse<List<ErrorResponse>>> handleNotFoundException(NotFoundException ex, HttpServletRequest request) {
+    protected ResponseEntity<GlobalResponse<List<ErrorResponse>>> handleNotFoundException(NotFoundException ex, HttpServletRequest request) {
         ErrorResponse error = ErrorResponse.builder()
                 .title("Entity not found")
                 .detail(ex.getMessage())
                 .build();
+
+        log.error(EXCEPTION_MESSAGE, ex.toString());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(GlobalResponseFactory.build(List.of(error), request.getRequestURI()));
     }
 
     @ExceptionHandler(EnumMappingException.class)
-    public ResponseEntity<GlobalResponse<List<ErrorResponse>>> handleEnumMappingException(EnumMappingException ex, HttpServletRequest request) {
+    protected ResponseEntity<GlobalResponse<List<ErrorResponse>>> handleEnumMappingException(EnumMappingException ex, HttpServletRequest request) {
         ErrorResponse error = ErrorResponse.builder()
                 .title("Enum mapping error")
                 .detail(ex.getMessage())
                 .build();
+
+        log.error(EXCEPTION_MESSAGE, ex.toString());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(GlobalResponseFactory.build(List.of(error), request.getRequestURI()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<GlobalResponse<List<ErrorResponse>>> handleDataIntegrityViolationException(DataIntegrityViolationException ex,
+                                                                                                     HttpServletRequest request) {
+        ErrorResponse error = ErrorResponse.builder()
+                .title("Internal server error")
+                .detail("There was an internal server error that couldn't be processed")
+                .build();
+
+        log.error(EXCEPTION_MESSAGE, ex.toString());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(GlobalResponseFactory.build(List.of(error), request.getRequestURI()));
@@ -63,6 +86,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                     .detail(detail)
                     .build());
         }
+
+        log.error(EXCEPTION_MESSAGE, ex.toString());
 
         ServletWebRequest servletWebRequest = (ServletWebRequest) request;
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
